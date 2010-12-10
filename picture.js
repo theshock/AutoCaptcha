a59a1e1ade90811276d6aae1c837463a20d18c93
Shock <shocksilien@gmail.com>
2010-12-10 03:11:45 +0200

    Start project

--- picture.js
+++ picture.js
@@ -7,70 +7,63 @@ var Color = function (r, g, b, a) {
 	this.a = a;
 };
 
-var Picture = function (canvas) {
-	this.canvas = picture.imageToCanvas(canvas);
-	this.canvas.ctx = this.canvas.getContext('2d');
-};
-
 var INVERT_2550 = 1/2550;
 
-Picture.prototype = {
-	getColors : function () {
-		return this.getPixelsCallback(function (r,g,b,a) {
-			return new Color(r,g,b,a);
-		});
-	},
-	getLuminance : function (asFloat) {
-		var round = Math.round;
-		return this.getPixelsCallback(function (r,g,b,a) {
-			var color = (r * 3 + g * 6 + b * 1);
-			return (asFloat) ? color * INVERT_2550 : round(color * 0.1);
-		});
-	},
-	getPixelsCallback : function (fn) {
-		var c = this.canvas, w = c.width, h = c.height;
-		var pix = c.ctx.getImageData(0,0,w,h).data;
-		var all = {}, x, y;
+var Picture = nano.implement(
+	function (canvas) {
+		this.canvas = Picture.imageToCanvas(canvas);
+		this.canvas.ctx = this.canvas.getContext('2d');
+	}, {
+		getColors : function () {
+			return this.getPixelsCallback(function (r,g,b,a) {
+				return new Color(r,g,b,a);
+			});
+		},
+		getLuminance : function (asFloat) {
+			var round = Math.round;
+			return this.getPixelsCallback(function (r,g,b,a) {
+				var color = (r * 3 + g * 6 + b * 1);
+				return (asFloat) ? color * INVERT_2550 : round(color * 0.1);
+			});
+		},
+		getPixelsCallback : function (fn) {
+			var c = this.canvas, w = c.width, h = c.height;
+			var pix = c.ctx.getImageData(0,0,w,h).data;
+			var all = {}, x, y;
 
-		for (y = 0; y < h; y++) for (x = 0; x < w; x++) {
-			if (!(y in all)) all[y] = {};
-			
-			var i = x*4 + y*4*w;
-			all[y][x] = fn(pix[i], pix[i+1], pix[i+2], pix[i+3]);
+			for (y = 0; y < h; y++) for (x = 0; x < w; x++) {
+				if (!(y in all)) all[y] = {};
+
+				var i = x*4 + y*4*w;
+				all[y][x] = fn(pix[i], pix[i+1], pix[i+2], pix[i+3]);
+			}
+			return all;
 		}
-		return all;
 	}
-};
+);
 
-var picture = function (canvas) {
-	return new Picture(canvas);
-};
-
-picture.isCanvas = function (elem) {
-	return elem instanceof HTMLCanvasElement;
-};
-
-picture.isImg = function (elem) {
-	return elem instanceof HTMLImageElement;
-};
-
-picture.imageToCanvas = function (elem) {
-	if (picture.isCanvas(elem)) {
-		return elem;
-	} else if (picture.isImg(elem)) {
-		var canvas = nano().create('canvas').get();
-		canvas.width  = elem.width;
-		canvas.height = elem.height;
-		canvas.getContext('2d').drawImage(elem, 0, 0);
-		return canvas;
-	} else {
-		nano.log('NotImageException: ', elem);
-		throw 'NotImage';
+window.Picture = nano.extend(Picture, {
+		isCanvas : function (elem) {
+			return elem instanceof HTMLCanvasElement;
+		},
+		isImg : function (elem) {
+			return elem instanceof HTMLImageElement;
+		},
+		imageToCanvas : function (elem) {
+			if (Picture.isCanvas(elem)) {
+				return elem;
+			} else if (Picture.isImg(elem)) {
+				var canvas = nano().create('canvas').get();
+				canvas.width  = elem.width;
+				canvas.height = elem.height;
+				canvas.getContext('2d').drawImage(elem, 0, 0);
+				return canvas;
+			} else {
+				nano.log('NotImageException: ', elem);
+				throw 'NotImage';
+			}
+		}
 	}
-};
-
-picture.fn = Picture.prototype;
-
-window.picture = picture;
+);
 
 })();
---
+++ tests/start/action.js
@@ -0,0 +1,17 @@
+
+if (window.fireunit) {
+	//fireunit.testDone();
+}
+
+nano(function () {
+	var l = new Picture(this.find('img').get()).getLuminance();
+	var lines = [], round = Math.round;
+	var box = function (l) {
+		return '█▓▒░ '[parseInt(l / 256 * 5)];
+	};
+	for (var y in l) for (var x in l[y]) {
+		if (!lines[y]) lines[y] = '';
+		lines[y] += box(l[y][x]);
+	}
+	nano('body').get().innerHTML = '<p>' + lines.join('<p>').replace(/\ /g, '&nbsp;');
+});
